1972839efb494b43b15196dc2f3431521b9ed906
Tyler <tylerhand.dev@gmail.com>
2022-10-03 21:08:10 -0500

    Added files

--- javascript.js
+++ javascript.js
@@ -1,10 +1,13 @@
-;(function($) {
-    "use strict";
-    $(document).ready(function() {
-        $('#access').on('touchstart click', '.skip-link', function(event) {
-            $(this).toggleClass('focus');
-            $($(this).attr('href')).toggleClass('target');
-            event.preventDefault();
-        }).find('.skip-link').append('<span>'+$('#menu .active').text()+'</span>');
-    });
-})(jQuery);
+const images=document.querySelectorAll("img");
+console.log(images.length)
+const imageContainer=document.querySelector(".slides");
+var  i=0;
+function start(){
+   i++;
+  console.log(i)
+  if (i>images.length-1){
+    i=0;
+  }
+  imageContainer.style.transform=`translateX(${-i*100}%)`
+}
+setInterval(start,2000);
